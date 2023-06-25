de671ae9c7826517df15c4fa6e54172bccae99bd
Lucas <lucasdevba@gmail.com>
2023-06-25 17:31:23 -0300

    Created - Pages and include routes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,24 +2,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const buttonClicked = useSelector((state) => state.example.buttonClicked);
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
+      <Link to="/register">
+        <FaUserAlt size={24} />
+      </Link>
       <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      <Link to="/estudante">
-        <FaUserAlt size={24} />
-      </Link>
-      {buttonClicked ? 'Clicked' : 'No clicked' }
     </Nav>
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
+import Student from '../pages/Student';
+import Students from '../pages/Students';
+import Register from '../pages/Register';
+import Files from '../pages/Files';
 import Page404 from '../pages/Page404';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Students />} />
+      <Route path="/student/:id/edit" element={<Student />} />
+      <Route path="/student/" element={<Student />} />
+      <Route path="/files/:id" element={<Files />} />
+      <Route path="/register/" element={<Register />} />
+      <Route path="/login/" element={<Login />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
